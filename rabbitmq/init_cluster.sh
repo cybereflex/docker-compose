bfc5a6e9ec306c9ea82ee1006a5ac8de8e1f5abf
cybereflex <chaosgensokyo@gmail.com>
2025-01-09 15:50:54 +0800

    rabbitmq support cluster, update snail-job to fix wecom alarm

---
+++ rabbitmq/init_cluster.sh
@@ -0,0 +1,2 @@
+docker exec -it rabbitmq-cluster-02 bash -c "rabbitmqctl stop_app && rabbitmqctl join_cluster rabbitmq@rabbitmq-cluster-01 && rabbitmqctl start_app"
+docker exec -it rabbitmq-cluster-03 bash -c "rabbitmqctl stop_app && rabbitmqctl join_cluster rabbitmq@rabbitmq-cluster-01 && rabbitmqctl start_app"
